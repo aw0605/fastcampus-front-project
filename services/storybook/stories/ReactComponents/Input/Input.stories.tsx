04d39d47ad65ea0445008751b429633791a59983
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-09-14 20:32:51 +0900

    Input 컴포넌트 생성

---
+++ services/storybook/stories/ReactComponents/Input/Input.stories.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "@fastcampus/react-components-input/style.css";
+import {
+  Input,
+  InputGroup,
+  InputLeftAddon,
+} from "@fastcampus/react-components-input";
+
+export default {
+  title: "React Components/Input",
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export const InputStory = {
+  render: () => <Input placeholder="dd" />,
+};
+
+export const InputGroupStory = {
+  render: () => (
+    <InputGroup size="lg" color="red">
+      <InputLeftAddon>+82</InputLeftAddon>
+      <Input placeholder="dd" />
+    </InputGroup>
+  ),
+};
