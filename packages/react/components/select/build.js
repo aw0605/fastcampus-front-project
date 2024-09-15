fa92f152b56bba470d816356703a0bf6c8d990cd
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-09-16 03:24:21 +0900

    Select 컴포넌트 생성

---
+++ packages/react/components/select/build.js
@@ -0,0 +1,21 @@
+import run from "@fastcampus/esbuild-config";
+import pkg from "./package.json" assert { type: "json" };
+import { vanillaExtractPlugin } from "@vanilla-extract/esbuild-plugin";
+import postcss from "postcss";
+import autoprefixer from "autoprefixer";
+
+const processCSS = async (css) => {
+  const style = await postcss([autoprefixer]).process(css, { from: undefined });
+
+  return style.css;
+};
+
+const config = {
+  plugins: [
+    vanillaExtractPlugin({
+      processCSS,
+    }),
+  ],
+};
+
+run({ pkg, config });
---
+++ packages/react/components/select/src/index.ts
@@ -0,0 +1,2 @@
+export type { SelectProps } from "./types";
+export { Select } from "./Select";
