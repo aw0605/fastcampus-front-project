ce8db1dbcfba3d432d79185395df2f6c06e0bce3
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-09-17 18:29:16 +0900

    아코디언 컴포넌트 테스트 코드

--- services/storybook/stories/ReactComponents/Accordion/Accordion.test.tsx
+++ services/storybook/stories/ReactComponents/Accordion/Accordion.test.tsx
@@ -2,11 +2,116 @@ import React from "react";
 import { describe, expect, it } from "vitest";
 
 import { render, screen } from "../../../test/test-utils";
+import { composeStories } from "@storybook/react";
+import * as stories from "./Accordion.stories";
 
-describe("test", () => {
-  it("test", () => {
-    render(<div>test</div>);
+const { AccordionTestStory } = composeStories(stories);
 
-    expect(screen.getByText("test")).toBeInTheDocument();
+describe("Accordion 컴포넌트 기능 테스트", () => {
+  it("Accordion 버튼 클릭 시, Panel이 펼쳐지는지 확인", async () => {
+    const args = {
+      items: [
+        {
+          name: "목록1",
+          content: "내용입니다.",
+        },
+      ],
+      defaultActiveItems: [],
+    };
+
+    render(<AccordionTestStory {...args} />);
+
+    expect(screen.getByTestId("panel-0")).toHaveAttribute(
+      "data-action-item",
+      "false",
+    );
+
+    await screen.getByTestId("button-0").click();
+    expect(screen.getByTestId("panel-0")).toHaveAttribute(
+      "data-action-item",
+      "true",
+    );
+  });
+
+  it("두번째 Accordion 버튼 클릭 시, 해당 Panel만 펼쳐지는지 확인", async () => {
+    const args = {
+      items: [
+        {
+          name: "목록1",
+          content: "목록1 내용입니다.",
+        },
+        {
+          name: "목록2",
+          content: "목록2 내용입니다.",
+        },
+      ],
+      defaultActiveItems: [],
+    };
+
+    render(<AccordionTestStory {...args} />);
+
+    expect(screen.getByTestId("panel-0")).toHaveAttribute(
+      "data-action-item",
+      "false",
+    );
+    expect(screen.getByTestId("panel-1")).toHaveAttribute(
+      "data-action-item",
+      "false",
+    );
+
+    await screen.getByTestId("button-1").click();
+    expect(screen.getByTestId("panel-0")).toHaveAttribute(
+      "data-action-item",
+      "false",
+    );
+    expect(screen.getByTestId("panel-1")).toHaveAttribute(
+      "data-action-item",
+      "true",
+    );
+  });
+
+  it("활성화 된 Accordion 버튼 클릭 시 Panel 접히는지 확인", async () => {
+    const args = {
+      items: [
+        {
+          name: "목록1",
+          content: "목록1 내용입니다.",
+        },
+      ],
+      defaultActiveItems: [],
+    };
+
+    render(<AccordionTestStory {...args} />);
+    await screen.getByTestId("button-0").click();
+
+    expect(screen.getByTestId("panel-0")).toHaveAttribute(
+      "data-action-item",
+      "true",
+    );
+
+    await screen.getByTestId("button-0").click();
+    expect(screen.getByTestId("panel-0")).toHaveAttribute(
+      "data-action-item",
+      "false",
+    );
+  });
+
+  it("defaultActiveItems에 itemName이 있을 경우, 초기 렌더링 시 Panel 펼쳐져 있는지 확인", async () => {
+    const args = {
+      items: [
+        {
+          name: "목록1",
+          content: "목록1 내용입니다.",
+        },
+      ],
+      defaultActiveItems: ["목록1"],
+    };
+
+    render(<AccordionTestStory {...args} />);
+
+    expect(screen.getByTestId("panel-0")).toHaveAttribute(
+      "data-action-item",
+      "true",
+    );
   });
 });
