afc77acd927c431ffedc836c529e4cd8d449424d
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-09-22 20:31:24 +0900

    프로젝트 세팅 및 환경설정 - Next.js

---
+++ services/page-builder/src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Document = () => {
+  return (
+    <Html lang="ko">
+      <Head />
+      <body className="antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
