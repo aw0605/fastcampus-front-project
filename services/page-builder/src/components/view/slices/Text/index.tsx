b0e6721820b673423b7dd41743101d9959a9f206
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-09-26 20:06:32 +0900

    강의 목록 Section
    - Swiper를 이용한 슬라이드 효과

--- services/page-builder/src/components/view/slices/Text/index.tsx
+++ services/page-builder/src/components/view/slices/Text/index.tsx
@@ -1,5 +1,6 @@
 import { Text } from "@fastcampus/react-components-layout";
 import { vars } from "@fastcampus/themes";
+import { useMemo } from "react";
 
 type Props = {
   text: string;
@@ -36,24 +37,31 @@ export const TextSlice: React.FC<Props> = ({
     highlightTextWeight,
   } = sliceStyle ?? {};
 
-  const regex = new RegExp(`(${highlightTexts.join("|")})`, "gi");
+  const hasHighlightText = highlightTexts.length > 0;
 
-  const highlightedText = text.split(regex).map((word, index) => {
-    if (highlightTexts.some((query) => new RegExp(query, "i").test(word))) {
-      return (
-        <span
-          key={`${word}-${index}`}
-          style={{
-            color: highlightTextColor,
-            fontWeight: highlightTextWeight ?? textWeight,
-          }}
-        >
-          {word}
-        </span>
-      );
+  const highlightedText = useMemo(() => {
+    if (hasHighlightText) {
+      const regex = new RegExp(`(${highlightTexts.join("|")})`, "gi");
+      return text.split(regex).map((word, index) => {
+        if (highlightTexts.some((query) => new RegExp(query, "i").test(word))) {
+          return (
+            <span
+              key={`${word}-${index}`}
+              style={{
+                color: highlightTextColor,
+                fontWeight: highlightTextWeight ?? textWeight,
+              }}
+            >
+              {word}
+            </span>
+          );
+        }
+
+        return word;
+      });
     }
-    return word;
-  });
+    return text;
+  }, [text, highlightTexts]);
 
   return (
     <Text
