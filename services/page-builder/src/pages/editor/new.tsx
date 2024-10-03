d01915ff7af02cc6f50324cbcc9e397fe79a6fef
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-10-03 16:34:21 +0900

    배포 기능 구현(1)
    - cloudflare workers, axios를 이용한 배포 기능 구현

--- services/page-builder/src/pages/editor/new.tsx
+++ services/page-builder/src/pages/editor/new.tsx
@@ -12,10 +12,12 @@ import { useViewSchemaValidation } from "@/src/hooks/useViewSchemaValidation";
 import { useToast } from "@fastcampus/react-components-toast";
 import { DesktopFirstSideNav } from "@/src/components/layout/DesktopFirstLayout/SideNav";
 import { JsonPresetList } from "@/src/components/EditorNewPage";
+import { putViewDetail } from "@/src/apis/worker/putViewDetail";
 
 const EditorNewPage: React.FC = () => {
   const { randomUUID } = new ShortUniqueId({ length: 10 });
-  const viewId = randomUUID();
+  const [viewId] = useState(randomUUID());
+  // const viewId = randomUUID();
 
   const { toast } = useToast();
 
@@ -48,6 +50,39 @@ const EditorNewPage: React.FC = () => {
     });
   };
 
+  const handlePublish = () => {
+    validateViewSchema({
+      viewSchema: schema,
+      onSuccess: async () => {
+        try {
+          await putViewDetail({
+            viewId,
+            data: {
+              value: JSON.stringify(schema),
+              metadata: {
+                createAt: new Date().toISOString(),
+              },
+            },
+          });
+        } catch (error) {
+          toast({
+            payload: {
+              // @ts-ignore
+              message: `[Fetch Error] ${error.message}`,
+            },
+          });
+        }
+      },
+      onError: ({ message }) => {
+        toast({
+          payload: {
+            message: message,
+          },
+        });
+      },
+    });
+  };
+
   return (
     <DesktopFirstLayout>
       <DesktopFirstNav gap={8}>
@@ -62,7 +97,7 @@ const EditorNewPage: React.FC = () => {
         >
           미리보기
         </Button>
-        <Button size="md" color="green">
+        <Button size="md" color="green" onClick={handlePublish}>
           배포하기
         </Button>
       </DesktopFirstNav>
