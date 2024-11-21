069784ea6b701749c85296dc7169432f6b6e63bf
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-11-22 01:46:30 +0900

    shorts 목록 API 구현

--- services/youtube/src/shared/mocks/handler.ts
+++ services/youtube/src/shared/mocks/handler.ts
@@ -2,11 +2,13 @@ import { HttpHandler } from "msw";
 import { getMockVideosPopularList } from "@/src/features/main/api/getVideoPopularList.mock";
 import { getMockSearchVideosList } from "@/src/features/search/api/getSearchVideosList.mock";
 import { getMockVideosDetail } from "../api/youtube/client/videoDetail/getVideoDetail.mock";
-import { getMockVideosDetailCommentList } from "../api/youtube/client/videoDetail/getVideosDetailCommentList.mock";
+import { getMockVideosDetailCommentList } from "@/src/shared/api/youtube/client/videoDetail/getVideosDetailCommentList.mock";
+import { getMockShrotsVideosList } from "@/src/features/videos/shorts/api/getShortsVideosList.mock";
 
 export const handlers: HttpHandler[] = [
   getMockVideosPopularList,
   getMockSearchVideosList,
   getMockVideosDetail,
   getMockVideosDetailCommentList,
+  getMockShrotsVideosList,
 ];
